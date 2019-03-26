4e80c54195b0573bbf43d5e59d800fa0c84cf46c
alexanderedomsky1998 <alexander.edomsky@yandex.ru>
2019-03-26 20:49:11 +0300

    Update DivisionTest.java

--- src/test/java/Testing/DivisionTest.java
+++ src/test/java/Testing/DivisionTest.java
@@ -10,14 +10,14 @@ public class DivisionTest {
 
     @Test(groups = "MultDiv")
     @Parameters({"value1", "value2"})
-    public void TestLongSum(long a, long b)
+    public void TestLongDIv(long a, long b)
     {
         Assert.assertEquals(a/b,calculator.div(a,b));
     }
 
     @Test(groups = "MultDiv")
     @Parameters({"value1", "value2"})
-    public void TestDoubleSum(double a, double b)
+    public void TestDoubleDiv(double a, double b)
     {
         Assert.assertEquals(a/b, calculator.div(a,b),0.1);
     }
