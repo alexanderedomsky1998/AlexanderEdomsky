e279fdff14a07d1639120303e4d3a8958b1ce9d1
alexanderedomsky1998 <alexander.edomsky@yandex.ru>
2019-03-30 16:22:49 +0300

    Create MultiplyTest.java

---
+++ src/test/java/test/auto/hw1/MultiplyTest.java
@@ -0,0 +1,24 @@
+package test.auto.hw1;
+
+import com.epam.tat.module4.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class MultiplyTest {
+    private final Calculator calculator = new Calculator();
+
+    @Test(groups = "MultDiv")
+    @Parameters({"value1", "value2"})
+    public void TestLongMult(long a, long b)
+    {
+        Assert.assertEquals(a*b,calculator.mult(a,b));
+    }
+
+    @Test(groups = "MultDiv")
+    @Parameters({"value1", "value2"})
+    public void TestDoubleMult(double a, double b)
+    {
+        Assert.assertEquals(a*b, calculator.mult(a,b),0.1);
+    }
+}
